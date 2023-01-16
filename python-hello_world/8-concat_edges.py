5e02911d44bbbb306bea97d8e188f315ab366f3b
paulinepar <paulineparmigiani@gmail.com>
2023-01-16 23:44:16 +0100

    insert word in string

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = "with Python"
-str = str[0:66] + " " + str1 + str[66:]
+str = str[0:66] + " " + "with Python" + str[66:]
 print(str)
