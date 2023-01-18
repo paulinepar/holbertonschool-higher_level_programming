83eec498ae13caece512b30080ae9f637a84311b
paulinepar <paulineparmigiani@gmail.com>
2023-01-18 14:31:44 +0100

    lower to upper

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -2,11 +2,11 @@
 def uppercase(str):
     # iterer la string
     for s in str:
-        #verifie si c'est une lower
+        # verifie si c'est une lower
         if ord(s) >= 97 and ord(s) <=122:
             transfo = ord(s) - 32
         else:
             transfo = ord(s)
 
         print("{}".format(chr(transfo)), end="")
-    print()
+print()
