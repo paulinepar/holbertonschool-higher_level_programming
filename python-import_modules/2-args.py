073d4d9749466a9fc1ad9987fbb88931166e2e44
paulinepar <paulineparmigiani@gmail.com>
2023-01-20 13:51:36 +0100

    Task 2 print numbers and lists of its arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,13 +3,12 @@ if __name__ == "__main__":
     from sys import argv
     length = len(argv)
 
-    
-    if length == 1:
-        print("{} arguments.".format(0))
-    elif length == 2:
-        print("{} argument:".format(length - 1))
-        print("1: {}".format(argv[1]))
-    elif length > 2:
-        print("{} arguments:".format((length - 1)))
-        for i in range(1, length):
+if length == 1:
+    print("{} arguments.".format(0))
+elif length == 2:
+    print("{} argument:".format(length - 1))
+    print("1: {}".format(argv[1]))
+elif length > 2:
+    print("{} arguments:".format((length - 1)))
+    for i in range(1, length):
             print("{}: {}".format(i, argv[i]))
