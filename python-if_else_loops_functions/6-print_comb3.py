3e07fccd2be0f710314f45fad3e4063d1b1d61cc
paulinepar <paulineparmigiani@gmail.com>
2023-01-20 16:42:22 +0100

    correct output

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,5 @@
 for i in range(9):
     for n in range(i + 1, 10):
         if i == 8:
-            print("{}{}".format(i, n))
-        print("{}{}".format(i, n), end=", ")
+            print("{:d}{:d}".format(i, n))
+        print("{:d}{:d}".format(i, n), end=", ")
