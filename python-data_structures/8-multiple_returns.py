11366af77e4a599bf2523800518c9bbe5b672584
paulinepar <paulineparmigiani@gmail.com>
2023-01-24 15:57:49 +0100

    Task 8 : returns a tuple with lenght and first ch

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    len_sentence = len(sentence)
+    if len == 0:
+        First == None
+    else:
+        First = sentence[0]
+    len_sentence = len(sentence), First
+    return len_sentence
