28b0ef347392604c591c71cf1eb03c9c3d5c3796
paulinepar <paulineparmigiani@gmail.com>
2023-01-24 16:22:00 +0100

    Task 9 :  finds the biggest integer of a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    lenght = len(my_list)
+    max = my_list[0]
+    if lenght == 0:
+        return None
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
+    
