27d1c70491a5f4e00185d15cbff731c59416a96d
paulinepar <paulineparmigiani@gmail.com>
2023-01-26 18:09:44 +0100

    Task 7 : replaces or adds key/value in a dictio

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if key == 0:
-        return
-    else:
-        a_dictionary.update({key: value})
+    a_dictionary.update({key: value})
     return a_dictionary
