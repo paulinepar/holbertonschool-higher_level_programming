5a9ed24880c664e1ced3d193cb6524b2272a3ca6
paulinepar <paulineparmigiani@gmail.com>
2023-01-26 19:01:59 +0100

    Task 10 : returns key with the biggest value

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -3,5 +3,5 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
     else:
-        max_value = max(a_dictionary)
+        max_value = max(a_dictionary, key=a_dictionary.get)
     return max_value
