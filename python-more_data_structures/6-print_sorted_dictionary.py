d5eb43d123780ad6c2e3334efdf66fd1ff66875d
paulinepar <paulineparmigiani@gmail.com>
2023-01-26 17:28:59 +0100

    Task : 6 prints a dictionary by ordered keys

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    key = dict(sorted(a_dictionary.items()))
-    print("{}".format(key))
+    for key, value in sorted(a_dictionary.items()):
+        print("{}: {}".format(key, value))
