bbc21abe7cb31fea286a932c06a453dcff1dea89
paulinepar <paulineparmigiani@gmail.com>
2023-01-27 16:53:18 +0100

    Task 2 : prints the first x elements

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -4,7 +4,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             try:
                 print("{:d}".format(my_list[i]), end="")
-                j += 1 
+                j += 1
             except (ValueError, TypeError):
                 print(end="")
         print("")
