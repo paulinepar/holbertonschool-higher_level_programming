3adaadec669807dcba236e09f1bff7d5460d86fd
paulinepar <paulineparmigiani@gmail.com>
2023-01-31 22:22:29 +0100

    Task 6 : Write a class Square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -29,7 +29,7 @@ class Square():
         return self.__position
 
     @position.setter
-    def position(self, value):    
+    def position(self, value):   
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value[0]) is not int or value[0] < 0:
@@ -43,7 +43,7 @@ class Square():
             return print()
         else:
             for esp in range(self.__position[1]):
-                    print(end="\n" if self.__position[1] > 0 else "")
+                print(end="\n" if self.__position[1] > 0 else "")
             for i in range(self.__size):
                 for esp in range(self.__position[0]):
                     print(end=" ")
