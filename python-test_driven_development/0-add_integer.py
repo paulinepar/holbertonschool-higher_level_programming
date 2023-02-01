ab213612234c502b7ad58cb88bc1c6e1f526a5a8
paulinepar <paulineparmigiani@gmail.com>
2023-02-01 14:57:28 +0100

    Task 0.

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -6,12 +6,11 @@
 
 def add_integer(a, b=98):
     """function addition"""
-    if a is float:
-        a = int()
-    if b is float:
-        b = int()
-    if a is not int or float:
-        raise TypeError("a must be an integer")
-    if b is not int or float:
-        raise TypeError("b must be an integer")
-    return a + b
+    try:
+        sum = a + b
+        return int((sum))
+    except TypeError:
+        if type(a) is not int:
+            return 'a must be an integer'
+        else:
+            return 'b must be an integer'
