901b14c4d6c63a924111b56af75bdc21d125a236
paulinepar <paulineparmigiani@gmail.com>
2023-02-03 19:28:19 +0100

    Task 8: Compare rectangles

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -72,11 +72,10 @@ class Rectangle():
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if not isinstance(rect_2, Rectangle):
-             raise TypeError("rect_2 must be an instance of Rectangle")
+            raise TypeError("rect_2 must be an instance of Rectangle")
         elif not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if Rectangle.area(rect_1) >= Rectangle.area(rect_2):
             return rect_1
         else:
             return rect_2
-        
