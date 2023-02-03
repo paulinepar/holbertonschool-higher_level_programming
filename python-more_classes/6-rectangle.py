e60966dbbea973e5a041c026fe3969f5cacf73d9
paulinepar <paulineparmigiani@gmail.com>
2023-02-03 18:17:32 +0100

    Task 6: A square is a rectangle

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle():
         An empty class Square.
     """
     number_of_instances = 0
-    
+
     def __init__(self, width=0, height=0):
         self.height = height
         self.width = width
