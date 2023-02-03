a7bfa60810d62c3ffa9a8a5b37061c16eea15b8b
paulinepar <paulineparmigiani@gmail.com>
2023-02-03 22:14:19 +0100

    Task 5 : Maw integer unittest

---
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Unittest of max_integer
+"""
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+
+    def test_just_element(self):
+        self.assertEqual(max_integer([3]), 3)
+
+    def test_positive_elements(self):
+        self.assertEqual(max_integer([1, 2, 3, 4, 5]), 5)
+
+    def test_negative_elements(self):
+        self.assertEqual(max_integer([-1, -2, -3, -5]), -1)
+
+    def test_neg_pos_elements(self):
+        self.assertEqual(max_integer([-1, 3, -1, 4]), 4)
+
+    def empty_test_list(self):
+        self.assertEqual(max_integer([]), None)
+    
+    if __name__ == '__main__':
+        unittest.main()
