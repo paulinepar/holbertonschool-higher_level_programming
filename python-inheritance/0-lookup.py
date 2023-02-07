f6a239cd81f681b793bedea51957a69c0104acab
paulinepar <paulineparmigiani@gmail.com>
2023-02-07 10:04:52 +0100

    Task 0: returns the list of available attributes

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+    create function that return the list of available attributes
+     and methods of an object
+'''
+
+
+def lookup(obj):
+    '''function lookup'''
+    return dir(obj)
