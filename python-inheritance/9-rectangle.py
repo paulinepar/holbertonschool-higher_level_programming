49c4a06d37de7d1f082978cec910b44cc7d6f885
paulinepar <paulineparmigiani@gmail.com>
2023-02-08 16:22:54 +0100

    Task 9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -11,11 +11,11 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__height = height
         self.__width = width
-        self.__height == BaseGeometry.integer_validator(self, 'height', self.__height)
-        self.__width == BaseGeometry.integer_validator(self, 'width', self.__width)
+        self.__height == BaseGeometry.integer_validator(self, 'height', height)
+        self.__width == BaseGeometry.integer_validator(self, 'width', width)
 
     def area(self):
         return self.__height * self.__width
-        
+
     def __str__(self):
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
