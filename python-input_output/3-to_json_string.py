5549e380fa7e8ed4dfa7c77d65596ba8775dd164
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 11:52:45 +0100

    Task 3: return JSON (string)

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 '''
-    Write a function that returns the JSON representation of an object (string):
+    Write a function that returns the JSON
+    representation of an object (string):
 '''
 
 
