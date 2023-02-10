38a2141b2c3ff0d9d310402f73e84c2f688e1b72
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 12:01:47 +0100

    Task 4: return an object (json)

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+    Write a function that returns an object (Python data structure)
+    represented by a JSON string:
+'''
+
+
+import json
+'''json import'''
+
+
+def from_json_string(my_str):
+    '''function that return json data'''
+    return json.dumps(my_str)
