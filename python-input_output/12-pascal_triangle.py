019f38a65f7430d3f0be16f0b5555e33d5a24de8
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 20:59:18 +0100

    Task 12 pascal triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''
+    Create a function def pascal_triangle(n): that returns a 
+    list of lists of integers representing the Pascal’s triangle of n:
+'''
+
+
+def pascal_triangle(n):
+    '''function that return triangle pascal'''
+    list = [1]
+    if n <= 0:
+        return list
+    for i in range(n):
+        print(list)
+        newlist=[]
+        newlist.append(list[0])
+        for i in range(len(list) - 1):
+            newlist.append(list[i] + list[i + 1])
+        newlist.append(list[- 1])
+        list= newlist
+    return newlist
