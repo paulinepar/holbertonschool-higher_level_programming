3e50173533327eaa33e1c933574ab6184243daeb
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 12:36:52 +0100

    Task 5: writes an Object a text file, using JSON

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+'''
+    Write a function that writes an Object to a text file, using a JSON representation:
+'''
+
+import json
+'''json import'''
+
+
+def save_to_json_file(my_obj, filename):
+    '''function write object, using json'''
+    with open(filename, 'w') as f:
+        f.write(json.dumps(my_obj))
