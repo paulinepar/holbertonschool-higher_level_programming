99da56e7a0beae673f40e72901b12abb7a8490dd
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 21:01:58 +0100

    Task 11

---
+++ python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    Write a class Student that defines a student
+'''
+
+
+class Student():
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        list = {}
+        if attrs is not None:
+            for i in attrs:
+                if i in self.__dict__:
+                    list[i] = self.__dict__[i]
+            return list
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for i in json:
+            if i in self.__dict__:
+                self.__dict__[i] = json[i]
+        return json
