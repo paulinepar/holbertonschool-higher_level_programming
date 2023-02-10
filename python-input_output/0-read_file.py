65362fc6240a328a3d64d8007cd61f14f7402f92
paulinepar <paulineparmigiani@gmail.com>
2023-02-10 11:07:06 +0100

    Task 0: ope and read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+    Write a function that reads a text file (UTF8) and prints it to stdout:
+'''
+
+
+def read_file(filename=""):
+    '''function read_file'''
+    with open('filename', encoding='UTF8') as f:
+        print(f.read())
