895b9fc777ffaf27dc0ff486a6245677fbf7b129
paulinepar <paulineparmigiani@gmail.com>
2023-02-13 14:57:35 +0100

    Task 2

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     '''class Rectangle that inherits from base'''
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(self)
+        super().__init__(id)
         self.__id = id
         self.__width = width
         self.__height = height
