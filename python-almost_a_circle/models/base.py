90b83475e3a81bb19036e10e05474f163ead81ab
paulinepar <paulineparmigiani@gmail.com>
2023-02-13 13:47:38 +0100

    Task 1

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -10,6 +10,7 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        self.id = id
-        Base.__nb_objects += 1
-        
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
