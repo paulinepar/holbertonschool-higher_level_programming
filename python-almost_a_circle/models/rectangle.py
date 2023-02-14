a7d7cdbe1ba3a38398d4210cb31d5983a96ee707
paulinepar <paulineparmigiani@gmail.com>
2023-02-14 11:28:50 +0100

    Task 7

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,4 +81,5 @@ class Rectangle(Base):
 
     def __str__(self):
         '''function update class rectangle'''
-        return (f'[Rectangle]({self.id}) {self.x}/{self.y} - {self.width}/{self.height}')
+        return (f'[Rectangle]({self.id}) {self.x}/{self.y} \
+        - {self.width}/{self.height}')
