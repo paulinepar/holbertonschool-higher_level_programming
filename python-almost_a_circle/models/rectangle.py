58d7c5ee2dcdc16d7ff3d2715937ee0b1f8560aa
paulinepar <paulineparmigiani@gmail.com>
2023-02-14 17:12:06 +0100

    Task 9

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -84,16 +84,20 @@ class Rectangle(Base):
         return (f'[Rectangle]({self.id}) {self.x}/{self.y} \
 - {self.width}/{self.height}')
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         '''function args'''
-        for arg in range(len(args)):
-            if args:
-                self.id = args[0]
-            if len(args) > 1:
-                self.width = args[1]
-            if len(args) > 2:
-                self.height = args[2]
-            if len(args) > 3:
-                self.x = args[3]
-            if len(args) > 4:
-                self.y = args[4]
+        if len(args) > 0:
+            for arg in range(len(args)):
+                if args:
+                    self.id = args[0]
+                if arg > 0:
+                    self.width = args[1]
+                if arg > 1:
+                    self.height = args[2]
+                if arg > 2:
+                    self.x = args[3]
+                if arg > 3:
+                    self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
