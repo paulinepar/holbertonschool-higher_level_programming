f776d2ea565bb6ea9652176a6996d08f795674be
paulinepar <paulineparmigiani@gmail.com>
2023-02-15 10:07:33 +0100

    Task 6 correct task6 str

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -81,8 +81,8 @@ class Rectangle(Base):
 
     def __str__(self):
         '''function update class rectangle'''
-        return (f'[Rectangle]({self.id}) {self.x}/{self.y} \
-- {self.width}/{self.height}')
+        return ('[Rectangle]({}) {}/{} - {}/{}'.format
+        (self.id, self.x, self.y, self.width, self.height))
 
     def update(self, *args, **kwargs):
         '''function args'''
