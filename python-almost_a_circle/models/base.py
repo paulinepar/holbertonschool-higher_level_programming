221e7c628cf011cbf52e8475767484d146d9b279
paulinepar <paulineparmigiani@gmail.com>
2023-02-15 16:45:43 +0100

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 '''
     Write the first class Base:
 '''
+import json
 
 
 class Base():
@@ -15,3 +16,9 @@ class Base():
         else:
             Base.__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        if list_dictionaries is None:
+            return ("[]")
+        else:
+            return json.dumps(list_dictionaries)
