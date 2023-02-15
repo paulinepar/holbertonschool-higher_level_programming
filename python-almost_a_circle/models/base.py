14c7799f453250c88470624e38e0702ebdb23257
paulinepar <paulineparmigiani@gmail.com>
2023-02-15 16:48:55 +0100

    Task 15

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -17,6 +17,7 @@ class Base():
             Base.__nb_objects += 1
             self.id = self.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         '''dict to json string'''
         if list_dictionaries is None:
