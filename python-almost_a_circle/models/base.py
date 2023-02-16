289f62668dbdec9f6f676bf91529d8b693515a91
paulinepar <paulineparmigiani@gmail.com>
2023-02-16 17:26:34 +0100

    Task 17

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,7 +38,7 @@ class Base():
 
     @staticmethod
     def from_json_string(json_string):
-        if json_string is None:
+        if json_string is None and json_string == "":
             return "[]"
         else:
             return json.loads(json_string)
