04a828e263e3420971066e71899b52e55d2f18dd
Pauline <paulineparmigiani@gmail.com>
2023-03-14 11:03:14 +0100

    create table with multiples rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -4,8 +4,8 @@ CREATE TABLE IF NOT EXISTS second_table
 (
     id INT,
     name VARCHAR(256),
-    score INT,
-)
+    score INT
+);
 INSERT INTO second_table (id, name, score) 
 VALUES
     (1, "John", 10),
