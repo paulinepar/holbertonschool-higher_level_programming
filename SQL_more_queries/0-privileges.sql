b277102ef59ac2041a3a9ba0bacece4b274e6734
Pauline <paulineparmigiani@gmail.com>
2023-03-15 09:32:27 +0100

    show all privileges users

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 --  lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
-GRANT SELECT ON user_0d_1 TO localhost;
-GRANT SELECT ON user_0d_2 TO localhost;
+SHOW GRANT FOR user_0d_1@localhost;
+SHOW GRANT FOR user_0d_2@localhost;
