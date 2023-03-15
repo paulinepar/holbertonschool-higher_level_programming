4e1667569207a993cc7b735c1505dfca8f68872a
Pauline <paulineparmigiani@gmail.com>
2023-03-15 09:53:01 +0100

    create database user permissions, pwd

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -6,4 +6,4 @@
 CREATE DATABASE IF  NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS user_0d_2@localhost;
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
-ALTER USER user_0d_2@hbtn_0d_2 IDENTIFIED BY 'user_0d_2_pwd';
+ALTER USER user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
