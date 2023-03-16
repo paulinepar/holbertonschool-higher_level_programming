922f908270c4976d14164cf6d102eb9b1755d2be
Pauline <paulineparmigiani@gmail.com>
2023-03-16 18:30:21 +0100

    list all genre linked to that show

--- SQL_more_queries/15-comedy_only.sql
+++ SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Write a script that lists all Comedy shows in the database hbtn_0d_tvshows.
---The tv _genres table contains only one record where name = Comedy (but the id can be different)
+-- The tv _genres table contains only one record where name = Comedy (but the id can be different)
 -- Each record should display: tv_shows.title
 -- Results must be sorted in ascending order by the show title
 -- You can use only one SELECT statement
