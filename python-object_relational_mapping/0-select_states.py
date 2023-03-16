2f1691dc86c310f4b75a5de403d3bdda827376a1
Pauline <paulineparmigiani@gmail.com>
2023-03-16 22:36:33 +0100

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,18 @@
     script that lists all states from the database hbtn_0e_0_usa
 '''
 
-import MySQLdb
-import sys
-
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+
+    # recuperation des arguments
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
     # connection a la base de données
-    db = MySQLdb.connect(host='localhost', port=3306,
-                        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host='localhost', port=3306, user=user, passwd=passwd, db=db)
 
     # creation de l'objet cursor
     cur = db.cursor()
