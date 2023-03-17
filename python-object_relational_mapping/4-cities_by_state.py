0b4798c7c500be6bd137adebefdbab3dcd1f58ae
Pauline <paulineparmigiani@gmail.com>
2023-03-17 23:10:25 +0100

    exe file

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -6,30 +6,19 @@ import sys
 
 if __name__ == "__main__":
 
-    # Recover argument from user
-    user = sys.argv[1]
-    pswd = sys.argv[2]
-    db_name = sys.argv[3]
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
 
-    # connect database
-    db = MySQLdb.connect(host='localhost', user=user,
-                         passwd=pswd, db=db_name, port=3306)
-
-    # create cursor
     cur = db.cursor()
-
-    # executing MySQL Queries in Python
-    querry = """SELECT cities.id, cities.name, states.name \
-            FROM cities \
-            LEFT JOIN states ON states.id = cities.state_id\
-            ORDER BY cities.id ASC"""
-    cur.execute(querry)
-
-    # display
-    all_cities = cur.fetchall()
-    for cities in all_cities:
-        print(cities)
-
-    # close cursor & db
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities LEFT JOIN states\
+                ON states.id = cities.state_id\
+                ORDER BY cities.id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
     cur.close()
     db.close()
