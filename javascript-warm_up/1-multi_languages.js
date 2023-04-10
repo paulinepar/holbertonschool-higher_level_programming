7727fade30630110f332f0639e2f1058d42c3f60
Pauline <paulineparmigiani@gmail.com>
2023-04-10 11:13:56 +0200

    add com

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-
+// declare variable
 const myVar = 'C is fun';
 const myVar1 = 'Python is cool';
 const myVar2 = 'JavaScript is amazing';
-
+// print all
 console.log(myVar);
 console.log(myVar1);
 console.log(myVar2);
