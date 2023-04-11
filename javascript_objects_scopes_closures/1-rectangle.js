55dbb686da005dcff16c1bfcf386caef0402e94f
Pauline <paulineparmigiani@gmail.com>
2023-04-11 16:13:40 +0200

    fix semi

--- javascript_objects_scopes_closures/1-rectangle.js
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -7,9 +7,9 @@ Initialize the instance attribute width with the value of w
 Initialize the instance attribute height with the value of h
 */
 class Rectangle {
-    constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
-    }
+  }
 }
 module.exports = Rectangle;
