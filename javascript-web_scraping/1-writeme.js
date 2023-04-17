befa8af5f4ff8d75288490c06c6c378510f5c707
Pauline <paulineparmigiani@gmail.com>
2023-04-17 11:43:53 +0200

    fix correct output

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 
-const argv = process.argv;
 const fs = require('fs');
-const file = argv[2];
-const data = argv[3];
+const file = process.argv[2];
+const data = process.argv[3];
 fs.writeFile(file, data, 'utf-8', (err) => {
   if (err) {
     console.log(err);
