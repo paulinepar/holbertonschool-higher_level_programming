5657173c693492ff9d280ca825e3de0ab18c6f8c
Pauline <paulineparmigiani@gmail.com>
2023-04-18 22:41:05 +0200

    get content of webpage and stores

--- javascript-web_scraping/5-request_store.js
+++ javascript-web_scraping/5-request_store.js
@@ -4,12 +4,12 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
-    if (error) {
-        console.error(error);
-    }
-    try {
-        fs.writeFile(process.argv[3], body, 'utf-8', function (err, result) { if (err) console.log(err); });
-    } catch (err) {
-        console.log(err);
-    }  
-    });
+  if (error) {
+    console.error(error);
+  }
+  try {
+    fs.writeFile(process.argv[3], body, 'utf-8', function (err, result) { if (err) console.log(err); });
+  } catch (err) {
+    console.log(err);
+  }
+});
