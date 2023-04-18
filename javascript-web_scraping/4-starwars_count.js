2b63fee4e2de206e7e54949a77045e55939df8c4
Pauline <paulineparmigiani@gmail.com>
2023-04-18 13:09:06 +0200

    fix issue for correct ouput

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
