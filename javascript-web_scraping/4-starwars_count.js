714e4ce0a5dc3358b729e8cc7c568cb4b016226c
Pauline <paulineparmigiani@gmail.com>
2023-04-18 11:26:35 +0200

    fix issue for correct ouput

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 
 const request = require('request');
-const id = '18';
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request.get(url, (error, response, body) => {
   if (error) {
     console.log(error);
   }
   const data = JSON.parse(body);
-  let nbr_of_movies = 0;
+  let NBR_OF_MOVIES = 0;
 
-  for (let film of data.results) {
+  for (const film of data.results) {
     if (film.characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      nbr_of_movies++;
+      NBR_OF_MOVIES++;
     }
   }
-  console.log(nbr_of_movies);
+  console.log(NBR_OF_MOVIES);
 });
