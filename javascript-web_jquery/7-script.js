fd73f03ef568eb068fef7ee68289e26fe7982ac6
Pauline <paulineparmigiani@gmail.com>
2023-04-19 11:30:00 +0200

    fetches the char name from url

---
+++ javascript-web_jquery/7-script.js
@@ -0,0 +1,9 @@
+/*
+Write a JavaScript script that fetches the character name from this
+URL: https://swapi-api.hbtn.io/api/people/5/?format=json
+The name must be displayed in the HTML tag DIV#character
+You can’t use document.querySelector to select the HTML tag
+*/
+$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function(data) {
+    $('DIV#character').text(data.name);
+});
