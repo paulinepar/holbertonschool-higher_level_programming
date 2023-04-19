216b80bc414172535542ecb76877f2d0bbc68f8c
Pauline <paulineparmigiani@gmail.com>
2023-04-19 10:37:43 +0200

    change text color header to red

---
+++ javascript-web_jquery/0-script.js
@@ -0,0 +1,4 @@
+/* Write a JavaScript script that updates the text color 
+of the <header> element to red (#FF0000): */
+const header = document.querySelector('HEADER');
+header.style.color = "#FF0000";
